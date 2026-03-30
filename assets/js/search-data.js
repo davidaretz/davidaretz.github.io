32b8225ec71aafbb2b79c53a7c99381c90adda94
davidaretz <davidaretz@icloud.com>
2026-03-30 19:54:23 +0000

    Deploying to gh-pages from @ davidaretz/davidaretz.github.io@f41ca2fdd088011905ebe314d26739a4a66cd234 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -532,7 +532,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/rendercv/rendercv_output/david_aretz_cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
